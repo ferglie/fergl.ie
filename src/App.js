1115a4fa632deb0cce6900be266ffa6d714e0b2a
Fergal Moran <fergal.moran@gmail.com>
2019-11-06 18:36:35 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/qr">
+          <Rick />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
+function Rick() {
+  window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  return null;
+}
+function Home(){
+  return <div className="App">
+    <div className="clouds"><div className="mover-1"></div></div>
+    <div className="clouds"><div className="mover-1"></div></div>
+    <div className="clouds"><div className="mover-1"></div></div>
+    <div id="super-wrapper">
+      <div id="wrapper" className="centered">
+        <header>
+          <img src="images/ferglier.gif" /><br />
+          <img src="images/thanyou.gif" /><br />
+        </header>
+        <a className="button" href="#" onClick={increaseTheMagic}>MOAR MAGIC</a>
+      </div>
+    </div>
+    <audio loop>
+      <source src="audio/power_of_love.mp3" type="audio/mpeg" />
+    </audio>
+  </div>;
+}
 export default App;
+
+function increaseTheMagic() {
+  document.querySelector('audio').play();
+}
