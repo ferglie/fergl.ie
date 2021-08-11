0db9c2410babd5beaf407c0efbe8d4f0b5061948
Fergal Moran <fergal.moran@gmail.com>
2021-08-11 14:45:53 +0100

    Fix lint issues

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-  Link
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -24,30 +18,42 @@ function App() {
   );
 }
 function Rick() {
-  window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
   return null;
 }
-function Home(){
-  return <div className="App">
-    <div className="clouds"><div className="mover-1"></div></div>
-    <div className="clouds"><div className="mover-1"></div></div>
-    <div className="clouds"><div className="mover-1"></div></div>
-    <div id="super-wrapper">
-      <div id="wrapper" className="centered">
-        <header>
-          <img src="images/ferglier.gif" /><br />
-          <img src="images/thanyou.gif" /><br />
-        </header>
-        <a className="button" href="#" onClick={increaseTheMagic}>MOAR MAGIC</a>
+function Home() {
+  return (
+    <div className="App">
+      <div className="clouds">
+        <div className="mover-1"></div>
       </div>
+      <div className="clouds">
+        <div className="mover-1"></div>
+      </div>
+      <div className="clouds">
+        <div className="mover-1"></div>
+      </div>
+      <div id="super-wrapper">
+        <div id="wrapper" className="centered">
+          <header>
+            <img alt="Ferglier Gif" src="images/ferglier.gif" />
+            <br />
+            <img alt="Than You Gif" src="images/thanyou.gif" />
+            <br />
+          </header>
+          <button className="button" onClick={increaseTheMagic}>
+            MOAR MAGIC
+          </button>
+        </div>
+      </div>
+      <audio loop>
+        <source src="audio/power_of_love.mp3" type="audio/mpeg" />
+      </audio>
     </div>
-    <audio loop>
-      <source src="audio/power_of_love.mp3" type="audio/mpeg" />
-    </audio>
-  </div>;
+  );
 }
 export default App;
 
 function increaseTheMagic() {
-  document.querySelector('audio').play();
+  document.querySelector("audio").play();
 }
