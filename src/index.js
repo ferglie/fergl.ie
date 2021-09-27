a05a99ad60a6037809870860ae4d0685a3e2d8ad
Fergal Moran <fergal.moran@gmail.com>
2021-09-27 09:39:31 +0100

    Add logs

--- src/index.js
+++ src/index.js
@@ -10,3 +10,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+console.log(
+  "%c🕒 Even a stopped clock is right twice a day ⏰",
+  "font-size:15px; background:#F9F9F9; color:#581845; padding:10px; border-radius:10px;"
+);
