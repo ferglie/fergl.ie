3138fc396b0821daa724de69975adff82791311b
Fergal Moran <fergal.moran@gmail.com>
2021-11-29 19:21:39 +0000

    Move to svelte kit

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,18 @@
+<script>
+	let count = 0;
+	try {
+		fetch('/visit').then(async (response) => {
+			const r = await response.json();
+			count = r.count;
+			console.log('index', 'count', r);
+		});
+	} catch (err) {
+		console.error('Error fetching component', err);
+	}
+</script>
+
+{#if count > 0}
+	<div>
+		You are visitor number: {count}
+	</div>
+{/if}
