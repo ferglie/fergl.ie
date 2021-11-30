7b7431bd9193be82cc7f17a7da308aa5a0ef6711
Fergal Moran <fergal.moran@gmail.com>
2021-11-30 11:00:17 +0000

    Added navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,40 @@
+<nav>
+	<div class="flex space-x-4 justify-center">
+		<a
+			href="/github"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+			>Github</a
+		>
+		<a
+			href="/youtube"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+			>YouTube</a
+		>
+		<a
+			href="/twitter"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+			>Twitter</a
+		>
+		<a
+			href="/mixcloud"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+			>Mixcloud</a
+		>
+		<a
+			href="/soundcloud"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+			>Soundcloud</a
+		>
+		<a
+			href="/mixes"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+			>Music</a
+		>
+	</div>
+</nav>
