c6501caecc1400dac9e0fc6cfd371d5f6839df5a
Fergal Moran <fergal.moran@gmail.com>
2022-02-19 19:20:54 +0000

    Add otherway

---
+++ src/routes/otherway/index.svelte
@@ -0,0 +1,131 @@
+<script>
+  import { MetaTags, JsonLd } from 'svelte-meta-tags';
+</script>
+
+<div class="w-full lg:max-w-full lg:flex">
+	<div class="" title="Otherway">
+		<img src="https://cdn.fergl.ie/public/otherway/woodford/woodford.jpg" alt="Otherway @ Woodford" />
+	</div>
+	<div
+		class="flex flex-col justify-between p-4 leading-normal bg-white rounded-b lg:rounded-b-none lg:rounded-r"
+	>
+		<div class="mb-8">
+			<div class="mb-2 text-xl font-bold text-gray-900">Radio Otherway Party & Fundraiser</div>
+			<p class="flex items-center mb-1 text-sm text-gray-600">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="w-6 h-6 mr-2 text-indigo-700"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+					/>
+				</svg>
+				Starts at: 5pm
+			</p>
+			<p class="flex items-center mb-1 text-sm text-gray-600">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="w-6 h-6 mr-2 text-orange-400"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M14.121 15.536c-1.171 1.952-3.07 1.952-4.242 0-1.172-1.953-1.172-5.119 0-7.072 1.171-1.952 3.07-1.952 4.242 0M8 10.5h4m-4 3h4m9-1.5a9 9 0 11-18 0 9 9 0 0118 0z"
+					/>
+				</svg>
+				Door tax: €10
+			</p>
+			<p class="flex items-center mb-1 text-sm text-gray-600">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="w-6 h-6 mr-2 text-blue-400"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"
+					/>
+				</svg>
+				Lineup
+			</p>
+			<ul class="mb-3 ml-12 text-sm text-gray-700 list-disc">
+				<li>Shane Johnson</li>
+				<li>Eddie Kay</li>
+				<li>Bloody G</li>
+				<li>Darren Kelly</li>
+				<li>Adam Dunbar</li>
+				<li>Lisa Oh</li>
+				<li>Fergal Moran</li>
+				<li>Gilbert</li>
+				<li>Jim X Comet</li>
+				<li>Sonny Emerald</li>
+				<li>Aidan Quigley</li>
+				<li>Diego Cesar</li>
+				<li>Eamon O' Sullivan</li>
+				<li>Jinty</li>
+				<li>Paul O' Driscoll</li>
+				<li>Eamon Hutton</li>
+				<li>Ian Richards</li>
+				<li>Gary Fitz</li>
+				<li>Kiearan O' Sullivan</li>
+				<li>Cully</li>
+				<li>Simon Barry</li>
+				<li>Alan Gordon</li>
+				<li>Gina Johnson</li>
+				<li>Paul Slattery</li>
+				<li>Jean Luc Razza</li>
+				<li>Declan O Neil</li>
+				<li>Ed Dunlea</li>
+				<li>LADRIN & BEHN</li>
+			</ul>
+			<p class="text-base text-gray-700">
+				Ian is bringing in the Big Danley rig to get the air moving!
+			</p>
+		</div>
+	</div>
+</div>
+<MetaTags
+	title="Radio Otherway @ Woodford"
+	description="Radio Otherway Party & Fundraiser @ Woodford Cork @ Woodford Cork."
+	canonical="https://fergl.ie/otherway/"
+	openGraph={{
+		url: 'https://fergl.ie/otherway/',
+		title: 'Radio Otherway @ Woodford',
+		description: 'Radio Otherway Party & Fundraiser @ Woodford Cork @ Woodford Cork',
+		images: [
+			{
+				url: 'https://cdn.fergl.ie/public/otherway/woodford/woodford.jpg',
+				width: 800,
+				height: 600,
+				alt: 'Og Image Alt'
+			}
+		],
+		site_name: 'Radio Otherway'
+	}}
+	twitter={{
+		handle: '@nooshoes',
+		site: '@site',
+		cardType: 'summary_large_image',
+		title: 'Radio Otherway @ Woodford',
+		description: 'Radio Otherway Party & Fundraiser @ Woodford Cork @ Woodford Cork.',
+		image: 'Radio Otherway Party & Fundraiser @ Woodford Cork @ Woodford Cork',
+		imageAlt: 'Radio Otherway Party'
+	}}
+	facebook={{
+		appId: '265928751906543'
+	}}
+/>
