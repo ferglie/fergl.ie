e3261e47fef5e8c80e13d80d718de478dec01d93
Fergal Moran <fergal.moran@gmail.com>
2022-11-21 12:00:39 +0000

    Upgrade sveltekit

--- src/lib/components/Counter.svelte
+++ src/lib/components/Counter.svelte
@@ -1,11 +1,11 @@
 <script>
 	let count = 0;
 	try {
-		fetch('/visit').then(async (response) => {
-			const r = await response.json();
-			count = r.count;
-			console.log('index', 'count', r);
-		});
+		// fetch('/visit').then(async (response) => {
+		// 	const r = await response.json();
+		// 	count = r.count;
+		// 	console.log('index', 'count', r);
+		// });
 	} catch (err) {
 		console.error('Error fetching component', err);
 	}
