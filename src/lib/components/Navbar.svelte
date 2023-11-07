a137752614bcd3b8dc4cff67dc92dfe5635c5268
Fergal Moran <fergal.moran@gmail.com>
2023-11-07 21:05:17 +0000

    Add icons to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,46 +3,60 @@
 		<a
 			href="/github"
 			target="_blank"
-			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-			>Github</a
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex items-center"
 		>
-		<a
-			href="/youtube"
-			target="_blank"
-			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-			>YouTube</a
-		>
-		<a
-			href="/mastodon"
-            rel="me"
-			target="_blank"
-			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-			>Mastodon</a
-		>
-		<a
-			href="/bluesky"
-            rel="me"
-			target="_blank"
-			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-			>Mastodon</a
+			<i class="fa-brands fa-github mr-1" />
+			Github</a
 		>
 		<a
 			href="/mixcloud"
 			target="_blank"
 			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-			>Mixcloud</a
+		>
+			<i class="fa-brands fa-mixcloud mr-1" />
+			Mixcloud</a
 		>
 		<a
 			href="/soundcloud"
 			target="_blank"
 			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-			>Soundcloud</a
+		>
+			<i class="fa-brands fa-soundcloud mr-1" />
+			Soundcloud</a
 		>
 		<a
 			href="/mixes"
 			target="_blank"
 			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-			>Music</a
+		>
+			<i class="fa-solid fa-headphones mr-1" />
+			Music</a
+		>
+		<a
+			href="/youtube"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex items-center"
+		>
+			<i class="fa-brands fa-youtube mr-1" />
+			YouTube
+		</a>
+		<a
+			href="/mastodon"
+			rel="me"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+		>
+			<i class="fa-brands fa-mastodon mr-1" />
+			Mastodon</a
+		>
+		<a
+			href="/bluesky"
+			rel="me"
+			target="_blank"
+			class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+		>
+			<i class="fa-solid fa-square mr-1" />
+			Bluesky</a
 		>
 	</div>
 </nav>
